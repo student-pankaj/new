8a110827e17e438ea5a3eb373d91ac1b4ec17fe7
pankaj katkar <pankajkatkar263@gmail.com>
2026-07-31 23:20:49 +0530

    sixth

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
 def addition(a, b):
     return a + b
+
+def subtraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        raise ValueError("Denominator cannot be zero.")
+    return a / b
